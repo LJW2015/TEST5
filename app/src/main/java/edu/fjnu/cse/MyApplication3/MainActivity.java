628c88fc9581357c4ea3eb86a4b4fb7e1cb2481a
ljw <1312776844@qq.com>
2018-05-06 20:58:55 +0800

    commit

---
+++ app/src/main/java/edu/fjnu/cse/MyApplication3/MainActivity.java
@@ -0,0 +1,36 @@
+package edu.fjnu.cse.MyApplication3;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.preference.PreferenceFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.fjnu.cse.preferencestutorial.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Display the fragment as the main content.
+        FragmentManager mFragmentManager = getFragmentManager();
+        FragmentTransaction mFragmentTransaction = mFragmentManager
+                .beginTransaction();
+        PrefsFragment mPrefsFragment = new PrefsFragment();
+        mFragmentTransaction.replace(android.R.id.content, mPrefsFragment);
+        mFragmentTransaction.commit();
+    }
+
+    public static class PrefsFragment extends PreferenceFragment {
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            // Load the preferences from an XML resource
+            addPreferencesFromResource(R.xml.preferences);
+        }
+    }
+}
